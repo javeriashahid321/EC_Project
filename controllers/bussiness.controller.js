c2ef768cc2f2c0184280b33e54df05afd50efd24
ummekulsoom <ummekulsoom215@gmail.com>
2025-03-09 21:18:10 +0500

    sql file added

--- controllers/bussiness.controller.js
+++ controllers/bussiness.controller.js
@@ -10,54 +10,67 @@ module.exports.signup = async function (req, res, next) {
         await db.query('INSERT INTO user (accountname,email,accountpassword,userrole) VALUES (?,?,?,?)', [bname, bemail, hashpassword, role])
         const userId = await db.query('SELECT id FROM user WHERE email=?', [bemail])
         const id = userId[0][0].id
-        const token = jwt.sign({ id:id, role: role }, 'secretkey', (err, token) => {
+        const token = jwt.sign({ id: id, role: role }, 'secretkey', (err, token) => {
             res.status(200).json({
                 token,
                 message: 'bussiness registered successfully'
             })
         })
-    } catch (err){
+    } catch (err) {
         next(err)
     }
 }
-module.exports.login=async function (req,res,next) {
-    const {bemail,bpassword}=req.body
+module.exports.login = async function (req, res, next) {
+    const { bemail, bpassword } = req.body
     console.log(req.body)
-    const role='bussiness'
-    try{
-        const user=await db.query('SELECT id,accountpassword FROM user WHERE email=? AND userrole=?',[bemail,role])
+    const role = 'bussiness'
+    try {
+        const user = await db.query('SELECT id,accountpassword FROM user WHERE email=? AND userrole=?', [bemail, role])
         console.log(user)
-        const userId=user[0][0].id
-        if(user.length){
-            const validPassword=await bcrypt.compare(bpassword,user[0][0].accountpassword)
-            if(validPassword){
-                const role=user[0][0].userrole
-                const token=jwt.sign({id:userId,role:role},'secretkey')
+        const userId = user[0][0].id
+        if (user.length) {
+            const validPassword = await bcrypt.compare(bpassword, user[0][0].accountpassword)
+            if (validPassword) {
+                const role = user[0][0].userrole
+                const token = jwt.sign({ id: userId, role: role }, 'secretkey')
                 res.status(200).json({
                     token,
                     role,
-                    message:'bussiness logged in successfully'
+                    message: 'bussiness logged in successfully'
                 })
-            }else{
+            } else {
                 res.status(400).json({
-                    message:'Invalid password'
+                    message: 'Invalid password'
                 })
             }
         }
-    }catch(error){
+    } catch (error) {
         next(error)
     }
-    
+
 }
 
-module.exports.getCategories=async function (req,res,next){
-    try{
-        const categories=await db.query('SELECT * FROM categories')
+module.exports.getCategories = async function (req, res, next) {
+    try {
+        const categories = await db.query('SELECT * FROM categories')
         // console.log(typeof(categories[0]))
         res.status(200).json({
             categories
         })
-    }catch(error){
+    } catch (error) {
         next(error)
     }
 }
+module.exports.getCategoriesFields = async function (req, res, next) {
+    const { catagory } = req.params;
+    try {
+        const [rows] = await db.query('SELECT fields FROM categories WHERE name=?', [catagory]);
+        if (rows.length === 0) {
+            return res.status(404).json({ message: 'Category not found' });
+        }
+        res.status(200).json({ fields: rows[0].fields });
+    } catch (err) {
+        next(err);
+    }
+}
+
--- routes/bussiness.route.js
+++ routes/bussiness.route.js
@@ -5,6 +5,7 @@ const bussinessController=require('../controllers/bussiness.controller')
 router.post('/signup',bussinessController.signup)
 router.post('/login',bussinessController.login)
 router.get('/categories',bussinessController.getCategories)
+router.get('/getCatagorieFields/:catagory',bussinessController.getCategoriesFields)
 
 
 module.exports=router
